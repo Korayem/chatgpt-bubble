4943386d5dd7b9dc6d1d5ba9f8dcc56ed7704ba4
Salem Korayem <korayem83@gmail.com>
2024-04-26 14:48:48 +0300

    add model selection to options

--- js/optionsScript.js
+++ js/optionsScript.js
@@ -1,26 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const settingsForm = document.getElementById('settingsForm');
-    const apiKeyInput = document.getElementById('apiKeyInput');
-    const modelSelect = document.getElementById('modelSelect');
-    const serverURLInput = document.getElementById('serverURLInput');
-    const maxTokensInput = document.getElementById('maxTokensInput');
-    const temperatureInput = document.getElementById('temperatureInput');
+document.addEventListener( 'DOMContentLoaded', function ()
+{
+    const settingsForm = document.getElementById( 'settingsForm' );
+    const apiKeyInput = document.getElementById( 'openAIKey' );
+    const modelSelect = document.getElementById( 'openAIModel' );
+    const serverURLInput = document.getElementById( 'openRouterKey' );
+    const maxTokensInput = document.getElementById( 'maxTokens' );
+    const temperatureInput = document.getElementById( 'temperature' );
 
     // Load settings from storage and populate the form
-    function loadSettings() {
-        chrome.storage.sync.get(['userSettings'], function(result) {
-            if (result.userSettings) {
+    function loadSettings ()
+    {
+        chrome.storage.sync.get( [ 'userSettings' ], function ( result )
+        {
+            if ( result.userSettings )
+            {
                 apiKeyInput.value = result.userSettings.apiKey || '';
                 modelSelect.value = result.userSettings.model || '';
                 serverURLInput.value = result.userSettings.serverURL || '';
                 maxTokensInput.value = result.userSettings.max_tokens || '';
                 temperatureInput.value = result.userSettings.temperature || '';
             }
-        });
+        } );
     }
 
     // Save settings to storage
-    function saveSettings() {
+    function saveSettings ()
+    {
         const userSettings = {
             apiKey: apiKeyInput.value,
             model: modelSelect.value,
@@ -29,17 +34,19 @@ document.addEventListener('DOMContentLoaded', function() {
             temperature: temperatureInput.value
         };
 
-        chrome.storage.sync.set({ userSettings }, function() {
-            console.log('Settings saved');
-        });
+        chrome.storage.sync.set( { userSettings }, function ()
+        {
+            console.log( 'Settings saved' );
+        } );
     }
 
     // Event listener for form submission
-    settingsForm.addEventListener('submit', function(event) {
+    settingsForm.addEventListener( 'submit', function ( event )
+    {
         event.preventDefault();
         saveSettings();
-    });
+    } );
 
     // Initialize settings form on load
     loadSettings();
-});
+} );
